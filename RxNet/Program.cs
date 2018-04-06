b45b9a98aa963901f904b21857aae9e33f550d14
Carol <475880692@qq.com>
2018-04-06 12:38:25 +0800

    first commit

---
+++ RxNet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+namespace RxNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new Subject<string>();
+            WriteSequenceToConsole(subject);
+            subject.OnNext("a");
+            subject.OnNext("b");
+            subject.OnNext("c");
+            Console.ReadKey();
+        }
+        //Takes an IObservable<string> as its parameter. 
+        //Subject<string> implements this interface.
+        static void WriteSequenceToConsole(IObservable<string> sequence)
+        {
+            //The next two lines are equivalent.
+            //sequence.Subscribe(value=>Console.WriteLine(value));
+            sequence.Subscribe(Console.WriteLine);
+        }
+    }
+}
